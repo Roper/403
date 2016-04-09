c8c9f1cf49e342986fd29e48efd7be114a2772ea
Roper <772142597@qq.com>
2016-04-09 08:58:01 +0800

    jzoj 4304

--- Non Contest/4304.cpp
+++ Non Contest/4304.cpp
@@ -1,62 +1,72 @@
 #include <cstdio>
 #include <algorithm>
+using namespace std;
+typedef long long LL;
 
-const int N = 
-int n, q, tot;
-int a[N], aa[N], aaa[N], q[N], rt[N], lt[N], num[N], sum[N];
+const int N = 1e5*2;
+int n, Q;
+LL a[N], h[N+10][2], s[N], sum[N], lm[N], rm[N];
 
-bool cmp(int x, int y){return a[x] > a[y] ? 0 : 1;}
+int Hash(int x){
+	int t = x % N + 1;
+	while(h[t][0] != x && h[t][0]) t = t % N + 1;
+	h[t][0] = x;
+	return t;
+}
 
-int Half(int L, int R, int k){
+bool cmp(int x, int y){return h[x][0] < h[y][0];}
+
+int Half(int x){
+	int L = 1, R = n;
 	while(L <= R){
-		int mid = (L + R) / 2;
-		if(aaa[mid] >= k) R = mid - 1;
+		int mid = (L + R) >> 1;
+		if(h[a[mid]][0] >= x) R = mid - 1;
 		else L = mid + 1;
 	}
 	return L;
 }
 
 int main(){
-	fropen("a.in", "r", stdin), freopen("a.out", "w", stdout);
+	freopen("a.in", "r", stdin), freopen("a.out", "w", stdout);
 	
-	scanf("%d %d", &n, &q);
-	
+	scanf("%d %d", &n, &Q);
 	for(int i = 1; i <= n; i ++){
 		scanf("%d", &a[i]);
-		aa[i] = i;
-		while(tot && a[i] > q[tot]) rt[q[tot]] = i - 1, tot --;
-		q[++ tot] = i;
+		a[i] = Hash(a[i]);
 	}
-	while(tot) rt[q[tot --]] = n;
-	for(int i = n; i >= 1; i --){
-		while(tot && a[i] > q[tot]) lt[q[tot]] = i + 1, tot --;
-		q[++ tot] = i;
-	}
-	while(tot) lt[q[tot --]] = 1;
 	
-	sort(aa + 1, aa + n, cmp);
-	
-	tot = 0;
+	int top = 0; s[0] = 0;
 	for(int i = 1; i <= n; i ++){
-		if(i > 1 && aa[i] == aa[i - 1]) tot --;
-		num[++ tot] += (rt[i] - i + 1)*(i - lt[i] + 1);
-		aaa[tot] = aa[i];
+		while(top > 0 && h[a[s[top]]][0] <= h[a[i]][0]) top --;
+		s[++ top] = i;
+		lm[i] = i - s[top - 1];
 	}
-	for(int i = 1; i <= tot; i ++) sum[i] = sum[i - 1] + num[i];
+	top = 0; s[0] = n + 1;
+	for(int i = n; i >= 1; i --){
+		while(top > 0 && h[a[s[top]]][0] < h[a[i]][0]) top --;
+		s[++ top] = i;
+		rm[i] = s[top - 1] - i;
+		h[a[i]][1] += lm[i]*rm[i];
+	}
 	
-	for(int i = 1; i <= q; i ++){
-		char c; int x, pos;
-		c = getchar(), scanf("%d",&x);
-		pos = Half(1, tot, x);
-		if(c == '=')
-			if(aaa[pos] == k) printf("%d", sum[pos]);
-			else printf("0");
-		else if(c == '>')
-			if(aaa[pos] == k) printf("%d", sum[tot] - sum[pos]);
-			else printf("%d", sum[tot] - sum[pos - 1]);
-		else 
-			if(aaa[pos] == k) printf("%d", sum[pos - 1]);
-			else printf("%d", sum[pos]);
+	sort(a + 1, a + 1 + n, cmp);
+	for(int i = 1; i <= n; i ++)
+		sum[i] = sum[i - 1] + h[a[i]][1]*(h[a[i]][0] != h[a[i - 1]][0]);
+	scanf("\n");
+	for(int i = 1; i <= Q; i ++){
+		char c = getchar(); int x;
+		scanf("%d\n", &x);
+		int pos = Half(x);
+		if(x == h[a[pos]][0]){
+			if(c == '=') printf("%lld\n", h[a[pos]][1]);
+			else if(c == '>') printf("%lld\n", sum[n] - sum[pos]);
+			else printf("%lld\n", sum[pos - 1]);
+		}
+		else{
+			if(c == '=') printf("0\n");
+			else if(c == '>') printf("%lld\n", sum[n] - sum[pos - 1]);
+			else printf("%lld\n", sum[pos - 1]);
+		}
 	}
 	
 	return 0;
